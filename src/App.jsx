3332b4c08e523e973f9f98a9efaa3576d89931b7
sahad <sahadmuhammed289@gmail.com>
2025-04-25 22:45:30 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://www.docker.com/" target="_blank">
+          <img src="/image.png" className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React + Docker</h1>
+      <p className="read-the-docs">
+        Wanna know how to setup ? <a href="">Check out blog</a>
+      </p>
+    </>
+  );
+}
+
+export default App;
